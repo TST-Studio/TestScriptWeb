c78eb601de5fa5f16dbdfa4397510fcbfe7a2d66
Laura <lauraschlueter@ymail.com>
2025-08-25 12:44:25 -0700

    chore(routes): add route paths on main.tsx

---
+++ TestScriptWebPage/src/InstallationPage.tsx
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+
+const InstallationDetails = () => {
+    const navigate = useNavigate();
+
+    return (
+        <>
+        <div className="installation-detail-container">
+            <h2> TestScript Installation</h2>
+        </div>
+        </>
+    )
+
+};
+
+export default InstallationDetails;
