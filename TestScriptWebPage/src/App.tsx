1121a610caf5355e1e268912715d30ba28d14ee7
tuckerolsen <tuckerolsen23@gmail.com>
2025-08-26 08:05:22 -0700

    feat(front page): Added front page styling

--- TestScriptWebPage/src/App.tsx
+++ TestScriptWebPage/src/App.tsx
@@ -1,35 +1,40 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom' // dont erase this
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { useNavigate } from 'react-router-dom'; // dont erase this
+import './App.css';
 
 function App() {
   const navigate = useNavigate(); // dont erase this
 
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="learn-more"> {/* dont erase:  lines 22 & 23 take you to the installation page */}
-        <button onClick={() => navigate('/installation-page')}>Learn More</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className='header-buttons'>
+        <button
+          className='pill-btn'
+          onClick={() => navigate('/installation-page')}
+        >
+          Install
+        </button>
+
+        <button className='pill-btn' onClick={() => navigate('/about-page')}>
+          Meet the team
+        </button>
+      </header>
+      {/* <header>
+        <button
+          className='install'
+          onClick={() => navigate('/installation-page')}
+        >
+          Install
+        </button>
+
+        <button className='about-team' onClick={() => navigate('/about-page')}>
+          Meet the team
+        </button>
+      </header> */}
+      <h1 className='title'>TestScript</h1>
+      <h1 className='description'>An easy unit tester for a smoother flow.</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
