6d3bb08d6bf938489fcdc951354e14aaff3a1741
tuckerolsen <tuckerolsen23@gmail.com>
2025-08-26 10:49:39 -0700

    style(front-page):buttons animation

--- TestScriptWebPage/src/App.tsx
+++ TestScriptWebPage/src/App.tsx
@@ -1,42 +1,27 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom'; // dont erase this
 import './App.css';
+import './index.css';
 
 function App() {
   const navigate = useNavigate(); // dont erase this
 
   return (
     <>
-      <body className='body'>
-        <header className='header-buttons'>
-          <button
-            className='pill-btn'
-            onClick={() => navigate('/installation-page')}
-          >
-            Install
-          </button>
-
-          <button className='pill-btn' onClick={() => navigate('/about-page')}>
-            Meet the team
-          </button>
-        </header>
-        {/* <header>
+      <h1 className='header-buttons'>
         <button
-          className='install'
+          className='pill-btn'
           onClick={() => navigate('/installation-page')}
         >
           Install
         </button>
 
-        <button className='about-team' onClick={() => navigate('/about-page')}>
+        <button className='pill-btn' onClick={() => navigate('/about-page')}>
           Meet the team
         </button>
-      </header> */}
-        <h1 className='title'>Tst Studios</h1>
-        <h1 className='description'>
-          Creating solutions for a better tomorrow.
-        </h1>
-      </body>
+      </h1>
+      <h1 className='title'>Tst Studios.</h1>
+      <h1 className='description'>Creating solutions for a better tomorrow.</h1>
     </>
   );
 }
