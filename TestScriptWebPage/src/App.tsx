a99df0c66b9348a47ceaf88a38ec19e65304561c
tuckerolsen <tuckerolsen23@gmail.com>
2025-08-26 16:31:58 -0700

    feat:(front page) added video block

--- TestScriptWebPage/src/App.tsx
+++ TestScriptWebPage/src/App.tsx
@@ -8,24 +8,7 @@ function App() {
 
   return (
     <>
-
       <h1 className='header-buttons'>
-
-      <body className='body'>
-        <header className='header-buttons'>
-          <button
-            className='pill-btn'
-            onClick={() => navigate('/installation-page')}
-          >
-            Install
-          </button>
-
-          <button className='pill-btn' onClick={() => navigate('/about-team')}>
-            Meet the team
-          </button>
-        </header>
-        {/* <header>
-
         <button
           className='pill-btn'
           onClick={() => navigate('/installation-page')}
@@ -33,12 +16,24 @@ function App() {
           Install
         </button>
 
-        <button className='pill-btn' onClick={() => navigate('/about-page')}>
+        <button className='pill-btn' onClick={() => navigate('/about-team')}>
           Meet the team
         </button>
       </h1>
       <h1 className='title'>Tst Studios.</h1>
       <h1 className='description'>Creating solutions for a better tomorrow.</h1>
+      <h2>Background Video Example</h2>
+      <video
+        src='/videos/my-video.mp4' // put the file in /public/videos/
+        autoPlay
+        loop
+        muted
+        playsInline
+        style={{
+          width: '100%', // full width
+          height: 'auto', // maintain aspect ratio
+        }}
+      />
     </>
   );
 }
